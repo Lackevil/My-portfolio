c5fcbae40f925d56378f4d2d8c18ca207be6d660
Nazar Pasechnyi <whiteroot@protonmail.com>
2024-08-16 13:29:39 +0300

    fix bugs

--- js/main.js
+++ js/main.js
@@ -8,3 +8,12 @@ burger.addEventListener('click', () => {
   header.classList.toggle('active')
   body.classList.toggle('scroll-lock')
 })
+
+const headerLink = document.getElementsByClassName('header__link')
+
+for (let i = 0; i <= headerLink.length; i++) {
+  headerLink[i].addEventListener('click', () => {
+    body.classList.remove('scroll-lock')
+    header.classList.remove('active')
+  })
+}
